66d758492aff4b8a6113399c5b69f4cac85e9762
bankashi <trinigun.bankashi@gmail.com>
2024-10-14 22:59:25 -0500

    Mejoras en las conversiones

--- demo_webapp/utils/utils.py
+++ demo_webapp/utils/utils.py
@@ -1,4 +1,5 @@
 import requests
+from .formulas import celsius_a_fahrenheit, fahrenheit_a_celsius
 
 API_URL = "http://172.40.0.20:80/api/conversiones/temperatura"
 
@@ -11,11 +12,23 @@ def obtener_conversiones():
         print(f"Error al obtener conversiones: {e}")
         return []
 
-def crear_conversion(resultado, tipo):
+def crear_conversion(temperatura, tipo):
+    temperatura = float(temperatura)
+    if tipo == "F":
+        resultado = celsius_a_fahrenheit(temperatura)
+        tipo_completo = "Celsius a Fahrenheit"
+    elif tipo == "C":
+        resultado = fahrenheit_a_celsius(temperatura)
+        tipo_completo = "Fahrenheit a Celsius"
+    else:
+        print("Tipo de conversión no válido")
+        return False
+
     nueva_conversion = {
         "resultado": resultado,
-        "tipo": tipo
+        "tipo": tipo_completo
     }
+
     try:
         response = requests.post(API_URL, json=nueva_conversion)
         response.raise_for_status()
