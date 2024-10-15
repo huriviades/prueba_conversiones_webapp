d3e49d1e875f4fa58f62db0f5d5c72785e44dffc
bankashi <trinigun.bankashi@gmail.com>
2024-10-14 22:26:12 -0500

    Sensibilidad de los strict slashes"

--- demo_webapp/app.py
+++ demo_webapp/app.py
@@ -4,12 +4,12 @@ from utils.utils import obtener_conversiones, crear_conversion
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Necesaria para usar flash messages
 
-@app.route('/temperaturas', methods=['GET'])
+@app.route('/temperaturas/', methods=['GET'], strict_slashes=False)
 def get_temperaturas():
     conversiones = obtener_conversiones()
     return render_template('get_temperaturas.html', conversiones=conversiones)
 
-@app.route('/temperaturas/crear', methods=['GET', 'POST'])
+@app.route('/temperaturas/crear/', methods=['GET', 'POST'], strict_slashes=False)
 def create_temperatura():
     if request.method == 'POST':
         resultado = request.form['resultado']
